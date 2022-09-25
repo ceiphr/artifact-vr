87d740567295701c676fb4a397263ff0866639de
Ari <ari@ceiphr.com>
2022-09-24 22:19:54 -0400

    Bumped dep versions. Removed modules. Added gh actions build workflow.

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -1,12 +1,14 @@
-var descArr = ["Yes, this is also my APCS final.",
-	"Haha, yes. This is my APCS final too.",
-	"Tomatoes are high in protein and this is my APCS final as well."];
+var descArr = [
+    "Yes, this is also my APCS final.",
+    "Haha, yes. This is my APCS final too.",
+    "Tomatoes are high in protein and this is my APCS final as well.",
+];
 function metaDesc(desc) {
-	console.log(desc)
+    console.log(desc);
 }
 function getRandom(arr) {
-	var rand = Math.random();
-	return arr[Math.floor(rand * arr.length)];
+    var rand = Math.random();
+    return arr[Math.floor(rand * arr.length)];
 }
 var desc = getRandom(descArr);
 metaDesc(desc);
@@ -14,13 +16,13 @@ metaDesc(desc);
 // Toggle Text
 // Add onclick="methodName()" to html element attribute to be interacted with.
 var mtext1 = document.querySelector("#mText1");
-mtext1.object3D.visible = false;
+if (mtext1?.object3D.visible) mtext1.object3D.visible = false;
 var mtext2 = document.querySelector("#mText2");
-mtext2.object3D.visible = false;
+if (mtext2?.object3D.visible) mtext2.object3D.visible = false;
 var mtext3 = document.querySelector("#mText3");
-mtext3.object3D.visible = false;
-var mtext4 = document.querySelector("#fText1");
-fText1.object3D.visible = false;
+if (mtext3?.object3D.visible) mtext3.object3D.visible = false;
+var fText1 = document.querySelector("#fText1");
+if (fText1?.object3D.visible) fText1.object3D.visible = false;
 
 var mInteract1 = document.querySelector("#mInteract1");
 var mInteract2 = document.querySelector("#mInteract2");
@@ -32,30 +34,30 @@ var curVis = "";
 
 // Toggle UI text
 function toggleVis(text, interact) {
-	if (vis && curVis != text) {
-		curVis.object3D.visible = false;
-		text.object3D.visible = true;
-		curVis = text;
-	} else if (!vis) {
-		resetVis();
-		text.object3D.visible = true;
-		curVis = text;
-		vis = !vis;
-	} else if (vis) {
-		text.object3D.visible = false;
-		curVis = text;
-		vis = !vis;
-	}
+    if (vis && curVis != text) {
+        curVis.object3D.visible = false;
+        text.object3D.visible = true;
+        curVis = text;
+    } else if (!vis) {
+        resetVis();
+        text.object3D.visible = true;
+        curVis = text;
+        vis = !vis;
+    } else if (vis) {
+        text.object3D.visible = false;
+        curVis = text;
+        vis = !vis;
+    }
 }
 
 function resetVis() {
-	mtext1.object3D.visible = false;
-	mtext2.object3D.visible = false;
-	mtext3.object3D.visible = false;
+    mtext1.object3D.visible = false;
+    mtext2.object3D.visible = false;
+    mtext3.object3D.visible = false;
 }
 
 // Toggle audio
-var audio1 = new Audio('assets/sounds/Demercurio_Interview.ogg');
+var audio1 = new Audio("assets/sounds/Demercurio_Interview.ogg");
 
 var audioInteract1 = document.querySelector("#audioInteract1");
 
@@ -64,25 +66,25 @@ var curPlay = "";
 var curInteract = "";
 
 function toggleAudio(af, interact) {
-	if (playing && curPlay != af) {
-		curPlay.pause();
-		af.play();
-		curPlay = af;
-		curInteract = interact;
-	} else if (!playing) {
-		resetAudio();
-		af.play();
-		curPlay = af;
-		curInteract = interact;
-		playing = !playing;
-	} else if (playing) {
-		af.pause();
-		curPlay = af;
-		curInteract = interact;
-		playing = !playing;
-	}
+    if (playing && curPlay != af) {
+        curPlay.pause();
+        af.play();
+        curPlay = af;
+        curInteract = interact;
+    } else if (!playing) {
+        resetAudio();
+        af.play();
+        curPlay = af;
+        curInteract = interact;
+        playing = !playing;
+    } else if (playing) {
+        af.pause();
+        curPlay = af;
+        curInteract = interact;
+        playing = !playing;
+    }
 }
 
 function resetAudio() {
-	audio1.pause();
+    audio1.pause();
 }
